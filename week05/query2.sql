f077a78e4afe3797387b1bfa0699054d0518c5b2
00DV001 <navdevansh03@gmail.com>
2025-07-05 18:53:16 +0530

    task 2 output time and event based triggers

---
+++ week05/query2.sql
@@ -0,0 +1,6 @@
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Email) VALUES
+(11, 'Rahul', 'Verma', 20, 'rahul.verma20@gmail.com'),
+(12, 'Isha', 'Desai', 22, 'isha.desai89@outlook.com'),
+(13, 'Arjun', 'Kapoor', 21, 'arjun.kapoor17@yahoo.com'),
+(14, 'Tanya', 'Malhotra', 19, 'tanya.malhotra99@hotmail.com'),
+(15, 'Manav', 'Bose', 23, 'manav.bose92@rediffmail.com');
